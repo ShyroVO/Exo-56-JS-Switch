fe2eaffdbd054e82ef56203f39af0d4388f69cf1
ShyroVO <Bosquet.laurie@outlook.com>
2020-11-25 14:06:59 +0100

    0.1

---
+++ app.js
@@ -0,0 +1,30 @@
+let accesoire = prompt("Votre accessoire informatique");
+
+switch (accesoire) {
+    case "souris":
+            alert("j'utilise une souris");
+        break;
+
+    case "clavier":
+        alert("j'utilise un clavier");
+        break;
+
+    case "ordinateur":
+        alert("j'utilise un ordinateur");
+        break;
+
+    case "tapis":
+        alert("j'utilise un tapis");
+        break;
+
+    case "imprimante":
+        alert("j'utilise une imprimante");
+        break;
+
+    case "ondulateur":
+        alert("j'utilise un ondulateur");
+        break;
+
+    default :
+        alert("Accessoire Inconnu")
+}
